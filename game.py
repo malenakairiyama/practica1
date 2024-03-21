498e24c7d6ad60ccfd363c08866ee93e2e3006ba
malenakairiyama <malenakairiyamas@gmail.com>
2024-03-21 17:27:20 -0300

    modificación en regla del juego: mientras no se llegue al número máx de fallos el juego continua

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ words = ["python", "programación", "computadora", "código", "desarrollo",
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de intentos permitidos
-max_attempts = 10
+max_fallos = 10
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
@@ -13,7 +13,10 @@ print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
-for i in range(max_attempts):
+
+fallos = 0 #contador
+
+while fallos < max_fallos:
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
     
@@ -33,6 +36,8 @@ for i in range(max_attempts):
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
+        fallos+=1
+        
     # Mostrar la palabra parcialmente adivinada
     letters = []
     for letter in secret_word:
@@ -47,5 +52,5 @@ for i in range(max_attempts):
         print(f"¡Felicidades! Has adivinado la palabra secreta:{secret_word}")
         break
 else:
-    print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
+    print(f"¡Oh no! Has agotado tus {max_fallos} intentos.")
     print(f"La palabra secreta era: {secret_word}")
